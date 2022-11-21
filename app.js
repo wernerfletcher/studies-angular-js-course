ba1de75e30a4f67520f9fa1ff8dcd7dc95588b00
Werner Fletcher <werner@cetos.io>
2022-11-21 14:28:08 +0200

    Lecture 6

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 (function () {
     'use strict';
 
-    angular.module('firstApp', [])
-        .controller('firstController', function ($scope) {
-            $scope.name = 'Werner';
+    angular.module('nameCalculator', [])
+        .controller('nameCalculatorController', function ($scope) {
+            $scope.name = '';
+            $scope.numericNameValue = 0;
+            $scope.displayNumericValue = function () {
+                $scope.numericNameValue = calculateNumericValue($scope.name);
+            };
+
+            function calculateNumericValue(name) {
+                let value = 0;
+                for (let a = 0; a < name.length; a++) {
+                    value += name.charCodeAt(a);
+                }
+                return value;
+            }
         });
 })();
