234f66916e6721bad904a01116451b5c833936d1
Werner Fletcher <werner@cetos.io>
2022-11-22 11:08:09 +0200

    Lecture 10

--- app.js
+++ app.js
@@ -2,19 +2,29 @@
     'use strict';
 
     angular.module('nameCalculator', [])
-        .controller('nameCalculatorController', function ($scope) {
+        .controller('nameCalculatorController', CalculatorController);
+
+        CalculatorController.$inject = ['$scope', '$filter'];
+        function CalculatorController($scope, $filter) {
             $scope.name = '';
+            $scope.nameUpper = '';
             $scope.numericNameValue = 0;
+            $scope.numericNameValueUpper = 0;
             $scope.displayNumericValue = function () {
                 $scope.numericNameValue = calculateNumericValue($scope.name);
-            };
-
-            function calculateNumericValue(name) {
-                let value = 0;
-                for (let a = 0; a < name.length; a++) {
-                    value += name.charCodeAt(a);
-                }
-                return value;
             }
-        });
+            $scope.displayUpperCaseName = function () {
+                var upper = $filter('uppercase');
+                $scope.nameUpper = upper($scope.name);
+                $scope.numericNameValueUpper = calculateNumericValue($scope.nameUpper);
+            }
+        }
+
+        function calculateNumericValue(name) {
+            let value = 0;
+            for (let a = 0; a < name.length; a++) {
+                value += name.charCodeAt(a);
+            }
+            return value;
+        }
 })();
