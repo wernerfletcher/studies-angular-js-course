8ea3a7678a5c96ab6889cc3b5c8d889d489dccd4
Werner Fletcher <werner@cetos.io>
2022-11-22 16:31:27 +0200

    Lecture 11

--- app.js
+++ app.js
@@ -2,22 +2,35 @@
     'use strict';
 
     angular.module('nameCalculator', [])
-        .controller('nameCalculatorController', CalculatorController);
+        .controller('nameCalculatorController', calculatorController);
 
-        CalculatorController.$inject = ['$scope', '$filter'];
-        function CalculatorController($scope, $filter) {
+        calculatorController.$inject = ['$scope', '$filter'];
+        function calculatorController($scope, $filter) {
             $scope.name = '';
             $scope.nameUpper = '';
             $scope.numericNameValue = 0;
             $scope.numericNameValueUpper = 0;
+            $scope.buttonSelected = false;
+            $scope.iconPrefix = 'off';
+            
             $scope.displayNumericValue = function () {
                 $scope.numericNameValue = calculateNumericValue($scope.name);
             }
+
             $scope.displayUpperCaseName = function () {
                 var upper = $filter('uppercase');
                 $scope.nameUpper = upper($scope.name);
                 $scope.numericNameValueUpper = calculateNumericValue($scope.nameUpper);
             }
+
+            $scope.changeButtonState = function () {
+                $scope.buttonSelected = !$scope.buttonSelected;
+                $scope.iconPrefix = getIconPrefix($scope.buttonSelected);
+            }
+        }
+
+        function getIconPrefix(buttonSelected) {
+            return buttonSelected ? 'on' : 'off';
         }
 
         function calculateNumericValue(name) {
