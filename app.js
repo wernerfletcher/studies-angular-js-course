9e4f2b5d6bc18d2794292d8034386ed17aa7ef16
Werner Fletcher <werner@cetos.io>
2022-11-29 13:27:48 +0200

    Lecture 20

--- app.js
+++ app.js
@@ -19,7 +19,9 @@
 
     angular.module('nameCalculator', [])
         .controller('nameCalculatorController', calculatorController)
-        .filter('leet', LeetFilterFactory);
+        .controller('todoListController', todoListController)
+        .filter('leet', LeetFilterFactory)
+        .service('TodoService', TodoService);
 
     calculatorController.$inject = ['$scope', '$filter', '$timeout', 'leetFilter'];
     function calculatorController($scope, $filter, $timeout) {
@@ -61,19 +63,11 @@
 
         $scope.petList = petList;
         $scope.dogList = dogList;
-    }
-
-    function LeetFilterFactory() {
-        let output = '';
-        return function (input, target, replacement) {
-            output = input.replaceAll(target, replacement);
-            return output;
-        }
-    }
+    };
 
     function setIconPrefix(buttonSelected) {
         return buttonSelected ? 'on' : 'off';
-    }
+    };
 
     function calculateNumericValue(name) {
         let value = 0;
@@ -81,5 +75,49 @@
             value += name.charCodeAt(a);
         }
         return value;
-    }
+    };
+
+    function LeetFilterFactory() {
+        let output = '';
+        return function (input, target, replacement) {
+            output = input.replaceAll(target, replacement);
+            return output;
+        }
+    };
+
+    todoListController.$inject = ['TodoService'];
+    function todoListController(TodoService) {
+        let todoCtrl = this;
+
+        todoCtrl.itemName = "";
+        todoCtrl.itemQuantity = "";
+        todoCtrl.items = TodoService.getItems();
+
+        todoCtrl.addItem = function () {
+            TodoService.addItem(todoCtrl.itemName, todoCtrl.itemQuantity);
+            todoCtrl.itemName = "";
+            todoCtrl.itemQuantity = "";
+        };
+
+        todoCtrl.removeItem = function (index) {
+            TodoService.removeItem(index);
+        };
+    };
+
+    function TodoService() {
+        let service = this;
+        let todoItems = [];
+
+        service.addItem = function (name, quantity) {
+            todoItems.push({ 'name': name, 'quantity': quantity });
+        };
+
+        service.getItems = function () {
+            return todoItems;
+        };
+
+        service.removeItem = function (index) {
+            todoItems.splice(index, 1);
+        };
+    };
 })();
