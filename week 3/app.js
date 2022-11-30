a54624ab611db3efa40c239138260de7a916405e
Werner Fletcher <werner@cetos.io>
2022-11-30 15:45:48 +0200

    Lecture 24

---
+++ week 3/app.js
@@ -0,0 +1,100 @@
+(function () {
+    'use strict';
+
+    angular.module('shoppingList', [])
+        .controller('ShoppingListController', ShoppingListController)
+        .service('ShoppingListService', ShoppingListService)
+        .service('ShoppingListVerificationService', ShoppingListVerificationService);
+
+    ShoppingListController.$inject = ['ShoppingListService'];
+    function ShoppingListController(ShoppingListService) {
+        let shopCtrl = this;
+
+        shopCtrl.items = ShoppingListService.getItems();
+        shopCtrl.itemName = '';
+        shopCtrl.itemQuantity = '';
+        shopCtrl.errorMessage = '';
+
+        shopCtrl.addItem = function () {
+            ShoppingListService.addItem(shopCtrl.itemName, shopCtrl.itemQuantity);
+        };
+
+        shopCtrl.removeItem = function (index) {
+            ShoppingListService.removeItem(index);
+        };
+    };
+
+    ShoppingListService.$inject = ['$q', 'ShoppingListVerificationService'];
+    function ShoppingListService($q, ShoppingListVerificationService) {
+        let service = this;
+
+        let items = [];
+
+        service.addItem = function (itemName, itemQuantity) {
+            $q.all([
+                ShoppingListVerificationService.checkItemName(itemName),
+                ShoppingListVerificationService.checkItemQuantity(itemQuantity)
+            ])
+                .then(function (res) {
+                    let newItem = {
+                        'name': itemName,
+                        'quantity': itemQuantity
+                    };
+
+                    items.push(newItem);
+                })
+                .catch(function (err) {
+                    console.error(err);
+                });
+        };
+
+        service.removeItem = function (index) {
+            items.splice(index, 1);
+        };
+
+        service.getItems = function () {
+            return items;
+        };
+    };
+
+    ShoppingListVerificationService.$inject = ['$q', '$timeout'];
+    function ShoppingListVerificationService($q, $timeout) {
+        let service = this;
+
+        service.checkItemName = function (name) {
+            let deferred = $q.defer();
+            let result = {
+                'message': ''
+            };
+
+            $timeout(function () {
+                if (name.toLowerCase().indexOf('chips') === -1) {
+                    deferred.resolve(result);
+                } else {
+                    result.message = 'Not allowed to order that item!'
+                    deferred.reject(result);
+                }
+            }, 3000);
+
+            return deferred.promise;
+        };
+
+        service.checkItemQuantity = function (quantity) {
+            let deferred = $q.defer();
+            let result = {
+                'message': ''
+            };
+
+            $timeout(function () {
+                if (quantity < 5) {
+                    deferred.resolve(result);
+                } else {
+                    result.message = 'Not allowed to order that many items!';
+                    deferred.reject(result);
+                }
+            }, 1000);
+
+            return deferred.promise;
+        };
+    };
+})();
