c8548ef89378c3fb01c2e928bc42fa0db1c1c481
Werner Fletcher <werner@cetos.io>
2022-12-12 13:51:30 +0200

    Lecture 36

--- src/shopping-list/shopping-list-module.js
+++ src/shopping-list/shopping-list-module.js
@@ -1,13 +1,28 @@
 (function () {
     'use strict';
 
-    angular.module('ShoppingListApp', ['Spinner']);
+    angular.module('ShoppingListApp', ['Spinner', 'ui.router']);
 
     angular.module('ShoppingListApp')
-        .config(function () {
-            console.log('ShoppingListApp module config method');
-        })
+        .config(RoutesConfig)
         .run(function () {
             console.log('ShoppingListApp module run method');
-    });
+        });
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider']
+
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('tab1');
+
+        $stateProvider
+            .state('tab1', {
+                url: '/tab1',
+                templateUrl: 'src/shopping-list/tab1.html'
+            })
+            .state('tab2', {
+                url: '/tab2',
+                templateUrl: 'src/shopping-list/tab2.html'
+            });
+    }
 })();
