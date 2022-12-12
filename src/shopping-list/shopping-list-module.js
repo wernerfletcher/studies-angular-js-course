25ee357c6efe0368bc4c23df4fbc9c44a9564741
Werner Fletcher <werner@cetos.io>
2022-12-12 13:22:53 +0200

    Run & Config functions

--- src/shopping-list/shopping-list-module.js
+++ src/shopping-list/shopping-list-module.js
@@ -2,4 +2,12 @@
     'use strict';
 
     angular.module('ShoppingListApp', ['Spinner']);
+
+    angular.module('ShoppingListApp')
+        .config(function () {
+            console.log('ShoppingListApp module config method');
+        })
+        .run(function () {
+            console.log('ShoppingListApp module run method');
+    });
 })();
--- src/spinner/spinner.module.js
+++ src/spinner/spinner.module.js
@@ -2,4 +2,12 @@
     'use strict';
 
     angular.module('Spinner', []);
+
+    angular.module('Spinner')
+        .config(function () {
+            console.log('Spinner module config method');
+        })
+        .run(function () {
+            console.log('Spinner module run method');
+        });
 })();
